754947a33b93b722443f8d188e9f29e1deb6d627
surya <suryagithub1811@gmail.com>
2024-04-22 17:32:35 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 $( function() {
     $( "#tabs" ).tabs();
   } );
+
+let name = document.getElementById('nameinput');
+let email = document.getElementById('emailinput');
+let subject = document.getElementById('subjectinput');
+// let message = document.getElementById('messageinput');
+let sendbtn = document.getElementById('sendbtn');
+
+sendbtn.addEventListener("click",()=>{
+    emailjs.send("service_3op1nlv","template_pzr16ul",{
+      form_name : name.value,
+      email : email.value,
+      subject : subject.value,
+      // message : message.value
+    }.then(
+      alert("your message sent successfully")
+    )
+
+)
+  
+})
+  
+  
