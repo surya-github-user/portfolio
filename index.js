716d0f5597fb993597814fee340977ebc9b39745
surya <suryagithub1811@gmail.com>
2024-04-22 17:41:49 +0530

    portfolio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $( function() {
 let name = document.getElementById('nameinput');
 let email = document.getElementById('emailinput');
 let subject = document.getElementById('subjectinput');
-// let message = document.getElementById('messageinput');
+let message = document.getElementById('messageinput');
 let sendbtn = document.getElementById('sendbtn');
 
 sendbtn.addEventListener("click",()=>{
@@ -13,7 +13,7 @@ sendbtn.addEventListener("click",()=>{
       form_name : name.value,
       email : email.value,
       subject : subject.value,
-      // message : message.value
+      message : message.value
     }.then(
       alert("your message sent successfully")
     )
